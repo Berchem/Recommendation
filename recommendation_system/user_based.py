020f23c245416ed109ada82ddea3d2d82b0f9931
Berchem <34000467+Berchem@users.noreply.github.com>
2018-08-26 23:05:37 +0800

    v0.3.2

--- recommendation_system/user_based.py
+++ recommendation_system/user_based.py
@@ -1,47 +1,31 @@
-import math
-from collections import defaultdict
+from .core import *
 
 
-def dot(p, q):
-    return sum(map(lambda p_i, q_i: p_i * q_i, p, q))
-
-
-def similarity_calc(p, q):
-    return dot(p, q) / math.sqrt(dot(p, p) * dot(q, q))
-
-
-class Similarity:
+class UserBased(CollaborativeFiltering):
     def __init__(self):
-        self.unique = sorted(tuple({ele for data in self.dataset for ele in data}))
-        self.matrix = [[similarity_calc(p, q) for q in self._similarity_map()] for p in self._similarity_map()]
-
-    def _similarity_vector(self, data):
-        return [1 if ele in data else 0 for ele in self.unique]
-
-    def _similarity_map(self):
-        return map(self._similarity_vector, self.dataset)
+        CollaborativeFiltering.__init__(self)
 
     def most_similar_set_to(self, subset):
         pairs = []
-        for other_set, similarity in enumerate(self.matrix[subset]):
+        for other_set, similarity in enumerate(self.usr_matrix[subset]):
             if subset != other_set and similarity > 0:  # find other users
                 pairs += [(other_set, similarity)]  # with nonzero similarity
 
         return sorted(pairs,  # sort them by most similar
                       key=lambda x: x[-1], reverse=True)
 
-    def similar(self, subset, include_current_interests=False):
+    def user_based(self, subset, include_current_items=False):
         # sum up the similarities
         suggestions = defaultdict(float)
         for other_set, similarity in self.most_similar_set_to(subset):
             for ele in self.dataset[other_set]:
-                suggestions[ele] += similarity
+                suggestions[ele] = similarity
 
         # convert them to a sorted list
         suggestions = sorted(suggestions.items(), key=lambda x: x[-1], reverse=True)
 
         # and (maybe) exclude already-interests
-        if include_current_interests:
+        if include_current_items:
             return suggestions
 
         else:
